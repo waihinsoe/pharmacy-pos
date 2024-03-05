27a94a2422dec269f67c3778e983e30c5c1e6931
waihinsoe <waihinsoe332004@gmail.com>
2024-03-05 16:51:39 +0630

    change: pos ui unfinish

--- client/src/pages/sales/NewSale.tsx
+++ client/src/pages/sales/NewSale.tsx
@@ -1,13 +1,26 @@
-import { Flex, Segmented, Select } from "antd";
+import {
+  Badge,
+  Button,
+  Collapse,
+  Flex,
+  Select,
+  Tag,
+  Typography,
+  theme,
+} from "antd";
 import { PosTopBar } from "../../components/navbar/PosTopBar";
 import { useCategories } from "../../hooks/categories/useCategories";
 import { useAuth } from "../../context/AuthContext";
 import { Category } from "../../types";
 import { useState } from "react";
 import { ShowProducts } from "./ShowProducts";
+import { FaPlus } from "react-icons/fa";
+import { GiMoneyStack } from "react-icons/gi";
 
+const { useToken } = theme;
 export const NewSale = () => {
   const { token } = useAuth();
+  const { token: themeToken } = useToken();
   const [selectedProducts, setSelectedProducts] = useState([]);
   console.log(selectedProducts);
   const [selectedCategoryId, setSelectedCategoryId] = useState<number | null>(
@@ -25,11 +38,14 @@ export const NewSale = () => {
   return (
     <Flex vertical style={{ height: "100vh" }}>
       <PosTopBar />
-      <Flex style={{ flex: 1, backgroundColor: "pink" }}>
+      <Flex style={{ flex: 1, backgroundColor: "pink", height: "90%" }}>
         <Flex
           flex={3}
           vertical
-          style={{ padding: "15px", backgroundColor: "#dfdfdf" }}
+          style={{
+            padding: "15px",
+            backgroundColor: "#dfdfdf",
+          }}
           gap={16}
         >
           <Select
@@ -48,10 +64,80 @@ export const NewSale = () => {
           />
         </Flex>
         <Flex
+          vertical
+          gap={16}
           flex={2}
-          style={{ padding: "10px 15px", flex: 2, backgroundColor: "#f8f8f8" }}
+          style={{
+            padding: "15px",
+            flex: 2,
+            backgroundColor: "#f8f8f8",
+          }}
         >
-          order section
+          <Flex>
+            <Button
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}
+              type="primary"
+              icon={<FaPlus />}
+            >
+              Add customer
+            </Button>
+          </Flex>
+          <Flex vertical gap={4} flex={1} style={{ overflowY: "scroll" }}>
+            {selectedProducts.map((item: any) => {
+              return (
+                <Collapse
+                  size="small"
+                  items={[
+                    {
+                      key: "1",
+                      label: (
+                        <Flex justify="space-between" align="center">
+                          <Flex gap={16} align="center">
+                            <Badge
+                              count={item.count}
+                              style={{
+                                backgroundColor: themeToken.colorPrimary,
+                              }}
+                            />
+
+                            <Typography style={{ fontWeight: "500" }}>
+                              {item.name}
+                            </Typography>
+                          </Flex>
+                          <Tag
+                            color="green"
+                            icon={<GiMoneyStack size={16} />}
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              gap: 8,
+                            }}
+                          >
+                            {item.price}
+                          </Tag>
+                        </Flex>
+                      ),
+                      children: <p>{item.name}</p>,
+                    },
+                  ]}
+                />
+              );
+            })}
+            {!selectedProducts.length && <div>not items</div>}
+          </Flex>
+          <Flex>
+            <Button
+              style={{
+                width: "100%",
+              }}
+              type="primary"
+            >
+              Checkout
+            </Button>
+          </Flex>
         </Flex>
       </Flex>
     </Flex>
