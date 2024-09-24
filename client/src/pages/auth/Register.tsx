e5085c7c08cc461c4f0232c36b628a2876a411e9
waihinsoe <waihinsoe332004@gmail.com>
2024-09-24 20:26:51 +0630

    feat: fix some bug

--- client/src/pages/auth/Register.tsx
+++ client/src/pages/auth/Register.tsx
@@ -10,126 +10,149 @@ import { MdOutlineEmail } from "react-icons/md";
 const { Option } = Select;
 
 export const Register = () => {
-  const navigate = useNavigate();
-  const { setAuthenticationStatus, login } = useAuth();
-  const onFinish = async (values: any) => {
-    const newUser = {
-      name: values.name,
-      email: values.email,
-      contact_number: values.contact_number,
-      password: values.password,
+    const navigate = useNavigate();
+    const { setAuthenticationStatus, login, status } = useAuth();
+    const onFinish = async (values: any) => {
+        const newUser = {
+            name: values.name,
+            email: values.email,
+            contact_number: values.contact_number,
+            password: values.password,
+        };
+
+        try {
+            setAuthenticationStatus(AUTH_STATUS.PENDING);
+            const response = await axios.post(
+                `${config.apiBaseUrl}/auth/register`,
+                newUser,
+                {
+                    withCredentials: true,
+                }
+            );
+            setAuthenticationStatus(AUTH_STATUS.SUCCEEDED);
+            const { user, token, expiresAt } = response.data;
+
+            // use login also becasue of same data return
+            login(user, token, expiresAt);
+            navigate("/");
+        } catch (error) {
+            setAuthenticationStatus(AUTH_STATUS.FAILED);
+        }
     };
 
-    try {
-      setAuthenticationStatus(AUTH_STATUS.PENDING);
-      const response = await axios.post(
-        `${config.apiBaseUrl}/auth/register`,
-        newUser,
-        {
-          withCredentials: true,
-        }
-      );
-      setAuthenticationStatus(AUTH_STATUS.SUCCEEDED);
-      const { user, token, expiresAt } = response.data;
+    return (
+        <Flex
+            justify="center"
+            align="center"
+            style={{ height: "100vh", padding: 8 }}
+        >
+            <Form
+                name="normal_login"
+                className="login-form"
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                style={{ width: "100%", maxWidth: 300 }}
+            >
+                <Form.Item
+                    name="name"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your Username!",
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={
+                            <UserOutlined className="site-form-item-icon" />
+                        }
+                        placeholder="Username"
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="email"
+                    rules={[
+                        {
+                            type: "email",
+                            required: true,
+                            message: "The input is not valid E-mail!",
+                        },
+                    ]}
+                >
+                    <Input prefix={<MdOutlineEmail />} placeholder="Email" />
+                </Form.Item>
+                <Form.Item
+                    name="contact_number"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your phone number!",
+                        },
+                    ]}
+                >
+                    <Input
+                        addonBefore={prefixSelector}
+                        style={{ width: "100%" }}
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your Password!",
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={
+                            <LockOutlined className="site-form-item-icon" />
+                        }
+                        type="password"
+                        placeholder="Password"
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="agreement"
+                    valuePropName="checked"
+                    rules={[
+                        {
+                            validator: (_, value) =>
+                                value
+                                    ? Promise.resolve()
+                                    : Promise.reject(
+                                          new Error("Should accept agreement")
+                                      ),
+                        },
+                    ]}
+                >
+                    <Checkbox>
+                        I have read the <a href="">agreement</a>
+                    </Checkbox>
+                </Form.Item>
 
-      // use login also becasue of same data return
-      login(user, token, expiresAt);
-      navigate("/");
-    } catch (error) {
-      setAuthenticationStatus(AUTH_STATUS.FAILED);
-    }
-  };
-
-  return (
-    <Flex
-      justify="center"
-      align="center"
-      style={{ height: "100vh", padding: 8 }}
-    >
-      <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        style={{ width: "100%", maxWidth: 300 }}
-      >
-        <Form.Item
-          name="name"
-          rules={[{ required: true, message: "Please input your Username!" }]}
-        >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Username"
-          />
-        </Form.Item>
-        <Form.Item
-          name="email"
-          rules={[
-            {
-              type: "email",
-              required: true,
-              message: "The input is not valid E-mail!",
-            },
-          ]}
-        >
-          <Input prefix={<MdOutlineEmail />} placeholder="Email" />
-        </Form.Item>
-        <Form.Item
-          name="contact_number"
-          rules={[
-            { required: true, message: "Please input your phone number!" },
-          ]}
-        >
-          <Input addonBefore={prefixSelector} style={{ width: "100%" }} />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: "Please input your Password!" }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item
-          name="agreement"
-          valuePropName="checked"
-          rules={[
-            {
-              validator: (_, value) =>
-                value
-                  ? Promise.resolve()
-                  : Promise.reject(new Error("Should accept agreement")),
-            },
-          ]}
-        >
-          <Checkbox>
-            I have read the <a href="">agreement</a>
-          </Checkbox>
-        </Form.Item>
-
-        <Form.Item>
-          <Button
-            style={{ width: "100%" }}
-            type="primary"
-            htmlType="submit"
-            className="register-form-button"
-          >
-            Register
-          </Button>
-          Or <Link to="/login">Login here!</Link>
-        </Form.Item>
-      </Form>
-    </Flex>
-  );
+                <Form.Item>
+                    <Button
+                        style={{ width: "100%" }}
+                        type="primary"
+                        htmlType="submit"
+                        className="register-form-button"
+                        loading={status === AUTH_STATUS.PENDING}
+                    >
+                        Register
+                    </Button>
+                    Or <Link to="/login">Login here!</Link>
+                </Form.Item>
+            </Form>
+        </Flex>
+    );
 };
 
 const prefixSelector = (
-  <Form.Item name="prefix" noStyle>
-    <Select style={{ width: 70 }}>
-      <Option value="86">+86</Option>
-      <Option value="87">+87</Option>
-    </Select>
-  </Form.Item>
+    <Form.Item name="prefix" noStyle>
+        <Select style={{ width: 70 }}>
+            <Option value="86">+86</Option>
+            <Option value="87">+87</Option>
+        </Select>
+    </Form.Item>
 );
--- client/src/pages/reports/salesReport/MonthlyReport.tsx
+++ client/src/pages/reports/salesReport/MonthlyReport.tsx
@@ -4,70 +4,72 @@ import { useMonthlySalesReport } from "../../../hooks/salesReport/useSaleReport"
 import { ReportDateQuery } from "../../../types";
 import { Area, Column, Rose } from "@ant-design/plots";
 interface Props {
-  chart: string;
-  query: ReportDateQuery;
+    chart: string;
+    query: ReportDateQuery;
 }
 
 export const MonthlyReport = ({ query, chart }: Props) => {
-  const { token } = useAuth();
+    const { token } = useAuth();
 
-  const { data, isLoading } = useMonthlySalesReport(token || "", query);
+    const { data, isLoading } = useMonthlySalesReport(token || "", query);
 
-  if (isLoading) return <div>loading ...</div>;
-  const areaConfig = {
-    data,
-    xField: "month",
-    yField: "total_amount",
-    smooth: true,
-    point: {
-      size: 5,
-      shape: "diamond",
-    },
-    style: {
-      fill: "linear-gradient(-90deg, white 0%, darkgreen 100%)",
-    },
-    line: {
-      style: {
-        stroke: "darkgreen",
-        strokeWidth: 2,
-      },
-    },
-  };
+    if (isLoading) return <div>loading ...</div>;
+    const areaConfig = {
+        data,
+        xField: "month",
+        yField: "total_amount",
+        smooth: true,
+        point: {
+            size: 5,
+            shape: "diamond",
+        },
+        style: {
+            fill: "linear-gradient(-90deg, white 0%, darkgreen 100%)",
+        },
+        line: {
+            style: {
+                stroke: "darkgreen",
+                strokeWidth: 2,
+            },
+        },
+    };
 
-  const columnConfig = {
-    data,
-    xField: "month",
-    yField: "total_amount",
-    style: {
-      fill: "darkgreen",
-    },
+    const columnConfig = {
+        data,
+        xField: "month",
+        yField: "total_amount",
+        style: {
+            fill: "darkgreen",
+        },
 
-    legend: false,
-  };
+        legend: false,
+    };
 
-  const roseConfig = {
-    width: 720,
-    height: 720,
-    autoFit: false,
-    radius: 0.85,
-    data,
-    xField: "month",
-    yField: "total_amount",
-    colorField: "month",
-    scale: { y: { type: "sqrt" }, x: { padding: 0 } },
-    axis: false,
-    legend: { color: { length: 400, layout: { justifyContent: "center" } } },
-    tooltip: { items: [{ channel: "y", valueFormatter: "~s" }] },
-  };
-  return (
-    <div>
-      {chart === "Area" && <Area {...areaConfig} />}
+    const roseConfig = {
+        width: 720,
+        height: 720,
+        autoFit: false,
+        radius: 0.85,
+        data,
+        xField: "month",
+        yField: "total_amount",
+        colorField: "month",
+        scale: { y: { type: "sqrt" }, x: { padding: 0 } },
+        axis: false,
+        legend: {
+            color: { length: 400, layout: { justifyContent: "center" } },
+        },
+        tooltip: { items: [{ channel: "y", valueFormatter: "~s" }] },
+    };
+    return (
+        <div>
+            {chart === "Area" && <Area {...areaConfig} />}
 
-      {chart == "Column" && <Column {...columnConfig} />}
+            {chart == "Column" && <Column {...columnConfig} />}
 
-      <Flex justify="center">
-        {chart == "Rose" && <Rose {...roseConfig} />}
-      </Flex>
-    </div>
-  );
+            <div style={{ display: "grid", placeItems: "center" }}>
+                {chart == "Rose" && <Rose {...roseConfig} />}
+            </div>
+        </div>
+    );
 };
